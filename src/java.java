04201dd28cfa0c4fdea3fec0325c40c19e5a641b
itsmeacs01 <itsmeacs01@gmail.com>
2021-03-17 01:39:41 +0545

    little changes ♨︎

--- src/java.java
+++ src/java.java
@@ -6,6 +6,7 @@ JFrame f;
 
     public java(){
     f = new JFrame();
+    JButton bac = new JButton("AC");
       JButton b1 = new JButton("1");
       JButton b2 = new JButton("2");
       JButton b3 = new JButton("3");
@@ -17,18 +18,30 @@ JFrame f;
       JButton b9 = new JButton("9");
       JButton b0 = new JButton("0");
 
+      JButton ba = new JButton("+");
+      JButton bs = new JButton("-");
+      JButton bm = new JButton("*");
+      JButton bd = new JButton("/");
+      JButton be = new JButton("=");
+
 f.add(b1);
 f.add(b2);
 f.add(b3);
+f.add(bac);
 f.add(b4);
 f.add(b5);
 f.add(b6);
+f.add(ba);
 f.add(b7);
 f.add(b8);
 f.add(b9);
+f.add(bs);
+f.add(bd);
 f.add(b0);
+f.add(bm);
+f.add(be);
 
-f.setLayout(new GridLayout(4,3));
+f.setLayout(new GridLayout(4,4));
 f.setSize(250,250);
 f.setVisible(true);
 
